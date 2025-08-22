86582151ff744b9a7dc4e8e070811e8f113a4159
Deep-Kacha <deepnkacha2256@gmail.com>
2025-08-22 16:46:51 +0530

    -

---
+++ DeepKacha_23SOECE11022/Tutorial_2/T2Q1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepKacha_23SOECE11022.Tutorial_2
+{
+    internal class T2Q1
+    {
+        static void Main(string[] args)
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* check the boolean condition using if statement */
+            if (a < 20)
+
+            {
+                /* if condition is true then print the following */
+                Console.WriteLine("a is less than 20");
+            }
+
+            Console.WriteLine("value of a is : {0}", a);
+            Console.ReadLine();
+
+        }
+    }
+}
