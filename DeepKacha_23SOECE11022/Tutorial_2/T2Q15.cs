0ef831faf0f76f656c5fff278d76b698f886ddc4
Deep-Kacha <deepnkacha2256@gmail.com>
2025-08-24 21:34:10 +0530

    -

---
+++ DeepKacha_23SOECE11022/Tutorial_2/T2Q15.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace eepKacha_23SOECE11022.Tutorial_2
+{
+    class T2Q15
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+        }
+    }
+}
