37d5a10895093415d3815ccb6d32dc9459f4a6a8
Deep-Kacha <deepnkacha2256@gmail.com>
2025-08-26 20:59:37 +0530

    Create TAQ1.cs

---
+++ DeepKacha_23SOECE11022/Tutorial_Array/TAQ1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepKacha_23SOECE11022
+{
+    class TAQ1
+    {
+        static void Main()
+        {
+            // Declare and initialize an array
+            int[] arr = { 10, 20, 30, 40, 50 };
+
+            // Print each element of the array
+            Console.WriteLine("Array elements:");
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
