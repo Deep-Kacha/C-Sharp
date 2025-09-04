350cfd08d35aa1b1cefe788fcc16a4c46cce5966
Deep-Kacha <deepnkacha2256@gmail.com>
2025-09-04 11:36:30 +0530

    --

--- DesktopApp/Form2.cs
+++ DesktopApp/Form2.cs
@@ -36,17 +36,24 @@ namespace DesktopApp
         private void button1_Click(object sender, EventArgs e)
         {
             String conStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\C-Sharp\\DesktopApp\\Database1.mdf;Integrated Security=True";
-            SqlConnection con = new SqlConnection(conStr);
-            con.Open();
-            string query = "INSERT INTO Student(EnrollNo, Age, Name, City)  VALUES (@EnrollNo, @Age, @Name, @City)";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@EnrollNo",txtEnroll.Text);
-            cmd.Parameters.AddWithValue("@Age",txtAge.Text);
-            cmd.Parameters.AddWithValue("@Name",txtCity.Text);
-            cmd.Parameters.AddWithValue("@City", txtName.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "INSERT INTO Student(EnrollNo, Age, Name, City) VALUES (@EnrollNo, @Age, @Name, @City)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@EnrollNo", txtEnroll.Text);
+                    cmd.Parameters.AddWithValue("@Age", txtAge.Text);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text); // Fixed: was txtCity.Text
+                    cmd.Parameters.AddWithValue("@City", txtCity.Text); // Fixed: was txtName.Text
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message);
+                }
+            }
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -57,16 +64,24 @@ namespace DesktopApp
         private void button2_Click(object sender, EventArgs e)
         {
             String conStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\C-Sharp\\DesktopApp\\Database1.mdf;Integrated Security=True";
-            SqlConnection con = new SqlConnection(conStr);
-            con.Open();
-            string query = "(string query = \"UPDATE Student SET Age=@Age, Name=@Name, City=@City WHERE EnrollNo=@EnrollNo)";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@EnrollNo", txtEnroll.Text);
-            cmd.Parameters.AddWithValue("@Age", txtAge.Text);
-            cmd.Parameters.AddWithValue("@Name", txtCity.Text);
-            cmd.Parameters.AddWithValue("@City", txtName.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "UPDATE Student SET Age=@Age, Name=@Name, City=@City WHERE EnrollNo=@EnrollNo";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@EnrollNo", txtEnroll.Text);
+                    cmd.Parameters.AddWithValue("@Age", txtAge.Text);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@City", txtCity.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message);
+                }
+            }
         }
 
         private void Age_Click(object sender, EventArgs e)
@@ -82,15 +97,23 @@ namespace DesktopApp
         private void Delete_Click(object sender, EventArgs e)
         {
             String conStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\C-Sharp\\DesktopApp\\Database1.mdf;Integrated Security=True";
-            SqlConnection con = new SqlConnection(conStr);
-            con.Open();
-            string query = "(string query = \"DELETE FROM Student WHERE EnrollNo=@EnrollNo\";\r\n)";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@EnrollNo", txtEnroll.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "DELETE FROM Student WHERE EnrollNo=@EnrollNo";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@EnrollNo", txtEnroll.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message);
+                }
+            }
         }
-
+                
         private void Reset_Click(object sender, EventArgs e)
         {
             String conStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\C-Sharp\\DesktopApp\\Database1.mdf;Integrated Security=True";
@@ -101,6 +124,11 @@ namespace DesktopApp
             cmd.Parameters.AddWithValue("@EnrollNo", txtEnroll.Text);
             cmd.ExecuteNonQuery();
             con.Close();
+
+        }
+
+        private void txtEnroll_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
