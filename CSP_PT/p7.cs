ae18b24069bc6a678ba10175645dc67e354e6943
Deep-Kacha <deepnkacha2256@gmail.com>
2025-09-09 22:31:55 +0530

    -

---
+++ CSP_PT/p7.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//4 4 4
+//4 1 4
+//4 2 4
+//4 3 4
+//4 4 4
+
+//*
+//# *
+//* # *
+//# * # *
+
+namespace CSP_PT
+{
+    internal class P1
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int c = 1;
+            for(int i=1; i<=n; i++)
+            {
+                for (int i = 3; i<=n; j--)
+            }
+        }
+    }
+}
